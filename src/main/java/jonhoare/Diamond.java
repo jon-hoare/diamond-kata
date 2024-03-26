ff6c60e86a0b94bd2589add6092f158b40072ac5
Jonathan Hoare <jon.hoare@gmail.com>
2024-03-26 15:54:56 +0000

    Minor tidy-up

--- src/main/java/jonhoare/Diamond.java
+++ src/main/java/jonhoare/Diamond.java
@@ -25,27 +25,27 @@ public class Diamond {
     if (character == null || !character.matches("^[A-Z]$")) {
       throw new BadInput();
     }
-    int middleCharIndex = character.charAt(0) - 'A' + 1;
-    int diamondSize = middleCharIndex * 2 - 1;
+    int middleCharIndex = character.charAt(0) - 'A'; // A=0, B=1 etc.
+    int diamondSize = middleCharIndex * 2 + 1;
     String[] diamondArray = new String[diamondSize];
-    for (int i = 0; i < middleCharIndex; i++) {
+    for (int i = 0; i <= middleCharIndex; i++) {
       String line = getDiamondLine(i, middleCharIndex);
       diamondArray[i] = line;
-      if (i < middleCharIndex - 1) {
+      if (i < middleCharIndex) {
         diamondArray[diamondSize - 1 - i] = line;
       }
     }
     this.diamond = Arrays.asList(diamondArray);
   }
 
-  private static String getDiamondLine(int i, int middleCharIndex) {
+  private static String getDiamondLine(int i, int n) {
     // 'A' is a special case since it only appears once on the line
     if (i == 0) {
-      String spaces = SPACE.repeat(middleCharIndex - 1);
+      String spaces = SPACE.repeat(n);
       return spaces + "A" + spaces;
     }
     // All other chars appear twice
-    String spacesAtEnds = SPACE.repeat(middleCharIndex - 1 - i);
+    String spacesAtEnds = SPACE.repeat(n - i);
     String spacesInMiddle = SPACE.repeat(2 * i - 1);
     String character = String.valueOf((char) ('A' + i));
     return spacesAtEnds + character + spacesInMiddle + character + spacesAtEnds;
